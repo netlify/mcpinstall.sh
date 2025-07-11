e2d3f31d933aba0c80cd281f7e7b54bf9f804ca6
Sean Roberts <sean.roberts@netlify.com>
2025-07-11 12:24:59 -0400

    styling updates

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        cream: '#fdf6e3',
+        navy: '#1a2946',
+      },
+    },
+  },
+  plugins: [],
+};
